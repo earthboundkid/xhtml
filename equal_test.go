be23901692d172cff6a6f9cac928956b1b2ae1a6
Carlana <mail@carlana.net>
2024-12-25 09:53:55 -0500

    Add DeepEqual

---
+++ equal_test.go
@@ -0,0 +1,38 @@
+package xhtml_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/carlmjohnson/be"
+	"github.com/earthboundkid/xhtml"
+	"golang.org/x/net/html"
+)
+
+func TestDeepEqual(t *testing.T) {
+	cases := []struct {
+		a, b  string
+		equal bool
+	}{
+		{"", "", true},
+		{"<a></a>", "<b></b>", false},
+		{"<p>hello, world</p>", "<p></p>", false},
+		{
+			`<h1><a href="http://example.com">link</a></h1><div>boo</div>`,
+			`<h1><a href="http://example.com">link</a></h1><div>boo</div>`,
+			true,
+		},
+		{
+			`<h1><a href="http://example.com">link</a></h1><div>boo</div>`,
+			`<h1><a href="http://example.com/">link</a></h1><div>boo</div>`,
+			false,
+		},
+	}
+	for _, tc := range cases {
+		a, err := html.Parse(strings.NewReader(tc.a))
+		be.NilErr(t, err)
+		b, err := html.Parse(strings.NewReader(tc.b))
+		be.NilErr(t, err)
+		be.Equal(t, tc.equal, xhtml.DeepEqual(a, b))
+	}
+}
